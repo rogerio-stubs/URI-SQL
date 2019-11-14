b2fee03a2a29f603342632c01ed7157e82491dac
rogerio.stubs <rogerio.stubs@benner.com.br>
2019-11-14 09:18:53 -0300

    códigos antigos

---
+++ 2609.sql
@@ -0,0 +1,3 @@
+select categories.name, sum(products.amount) from products
+	join categories on categories.id = products.id_categories
+    group by categories.name;
---
+++ 2617.sql
@@ -0,0 +1,3 @@
+select products.name, providers.name from products
+	join providers on providers.id = products.id_providers
+	where providers.name = 'Ajax SA'
