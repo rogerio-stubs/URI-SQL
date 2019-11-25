db424bb04899dc14530ae539bb317961ba5cefcd
rogerio.stubs <rogerio.stubs@benner.com.br>
2019-11-25 09:03:36 -0300

    Novos exercícios

---
+++ 2623.sql
@@ -0,0 +1,4 @@
+select products.name, categories.name from products
+	join categories on categories.id = products.id_categories
+    where products.amount > 100 and categories.id in (1, 2, 3, 6, 9)
+    order by categories.id
---
+++ 2625.sql
@@ -0,0 +1,3 @@
+-- A máscara do CPF é parecida com: '000.000.000-00'.
+
+select natural_person.cpf from natural_person
